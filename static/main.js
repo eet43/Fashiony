36f4ba9800313fd942b7ba42822ec724645beedb
eet43 <1998opening@gmail.com>
2022-04-22 10:33:44 +0900

    change loacl branch

--- static/main.js
+++ static/main.js
@@ -5,7 +5,8 @@ window.onload = init = () => {
 }
 
 
-// 카카오 OAuth
+// 카카오 서버로 url 요청
+//올바른 url이 오기 전, 이동시키면 안되기 때문에 비동기 처리로 진행했다.
 const onKakao = async () => {
 
 	let url = await fetch("/oauth/url", {
@@ -13,11 +14,10 @@ const onKakao = async () => {
 		method: "GET"
 	})
 	.then(res => res.json())
+		//응답 값으로 카카오 서버로 요청할 수 있는 url이 온다
 	.then(res => res['kakao_oauth_url']);
-
-	console.log(url);
+	//그 url로 이동시킨다
 	window.location.replace(url);
-
 }
 
 init();
